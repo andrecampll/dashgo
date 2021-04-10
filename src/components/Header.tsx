5940f1b66f003b45019e37e820150d7fa09def3b
André <andrevictor50@gmail.com>
2021-04-10 14:38:28 -0300

    Finishing Header compoennt

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Text, Input, Icon } from "@chakra-ui/react";
-import { RiSearchLine } from 'react-icons/ri';
+import { Flex, Text, Input, Icon, HStack, Box, Avatar } from "@chakra-ui/react";
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri';
 
 export function Header() {
   return (
@@ -42,6 +42,35 @@ export function Header() {
         />
 
         <Icon as={RiSearchLine} />
+      </Flex>
+    
+      <Flex
+        align="center"
+        ml="auto"
+      >
+        <HStack
+          spacing="4"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize="20" />
+          <Icon as={RiUserAddLine} fontSize="20" />
+        </HStack>
+      
+        <Flex align="center">
+          <Box mr="4" textAlign="right">
+            <Text>André</Text>
+            <Text color="gray.300" fontSize="small">
+              andrevictor50@gmail.com
+            </Text>
+          </Box>
+
+          <Avatar size="md" name="André Victor" src="https://github.com/andrecampll.png"/>
+        </Flex>
       </Flex>
     </Flex>
   );
