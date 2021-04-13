ecf790c4cbf994312ec1d76f4a3b581594a65e51
André <andrevictor50@gmail.com>
2021-04-13 06:47:51 -0300

    Adding navigation flux

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,16 +1,20 @@
 import { ElementType } from "react";
-import { Text, Link, Icon, LinkProps as ChakraLinkProps } from "@chakra-ui/react";
+import { Text, Link as ChakraLink, Icon, LinkProps as ChakraLinkProps } from "@chakra-ui/react";
+import Link from 'next/link';
 
 type NavLinkProps = {
   icon: ElementType;
   children: string;
+  href: string
 } & ChakraLinkProps;
 
-export function NavLink({ icon, children, ...rest }: NavLinkProps) {
+export function NavLink({ icon, children, href, ...rest }: NavLinkProps) {
   return (
-    <Link display="flex" align="center" {...rest}>
-      <Icon as={icon} fontSize="20" />
-      <Text ml="4" fontWeight="medium">{children}</Text>
+    <Link href={href} passHref>
+      <ChakraLink display="flex" align="center" {...rest}>
+        <Icon as={icon} fontSize="20" />
+        <Text ml="4" fontWeight="medium">{children}</Text>
+      </ChakraLink>
     </Link>
   );
 }
