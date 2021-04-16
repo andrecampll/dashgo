a942ab07f6697b333c7f8635da814106271fa93b
André <andrevictor50@gmail.com>
2021-04-16 07:37:13 -0300

    Adding MirageJS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,50 +29,54 @@ export default function SignIn() {
   }
 
   return (
-    <Flex
-      w="100vw"
-      h="100vh"
-      align="center"
-      justify="center"
-    >
-      <Flex
-        as="form"
-        width="100%"
-        maxWidth={360}
-        bg="gray.800"
-        p="8"
-        borderRadius={8}
-        flexDir="column"
-        onSubmit={handleSubmit(handleSignIn)}
-      >
-        <Stack spacing="4">
-          <Input
-            name="email"
-            type="email"
-            label="E-mail"
-            error={formState.errors.email}
-            {...register('email')}
-          />
-          
-          <Input
-            name="password"
-            type="password"
-            label="Senha"
-            error={formState.errors.password}
-            {...register('password')}
-          />
-        </Stack>
+    <>
+      <title>Dashgo | Login</title>
 
-        <Button
-          type="submit"
-          mt="6"
-          colorScheme="pink"
-          size="lg"
-          isLoading={formState.isSubmitting}
+      <Flex
+        w="100vw"
+        h="100vh"
+        align="center"
+        justify="center"
+      >
+        <Flex
+          as="form"
+          width="100%"
+          maxWidth={360}
+          bg="gray.800"
+          p="8"
+          borderRadius={8}
+          flexDir="column"
+          onSubmit={handleSubmit(handleSignIn)}
         >
-          Entrar
-        </Button>
+          <Stack spacing="4">
+            <Input
+              name="email"
+              type="email"
+              label="E-mail"
+              error={formState.errors.email}
+              {...register('email')}
+            />
+            
+            <Input
+              name="password"
+              type="password"
+              label="Senha"
+              error={formState.errors.password}
+              {...register('password')}
+            />
+          </Stack>
+
+          <Button
+            type="submit"
+            mt="6"
+            colorScheme="pink"
+            size="lg"
+            isLoading={formState.isSubmitting}
+          >
+            Entrar
+          </Button>
+        </Flex>
       </Flex>
-    </Flex>
+    </>
   )
 }
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -30,80 +30,84 @@ export default function UserList() {
   });
 
   return (
-    <Box>
-      <Header />
+    <>
+      <title>Dashgo | Users</title>
 
-      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
-        <Sidebar />
-        
-        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
-          <Flex mb="8" justify="space-between" align="center">
-            <Heading size="lg" fontWeight="normal" >Usuários</Heading>
+      <Box>
+        <Header />
 
-            <Link href="/users/create" passHref>
-              <Button
-                as="a"
-                size="sm"
-                fontSize="sm"
-                colorScheme="pink"
-                leftIcon={<Icon as={RiAddLine} fontSize="20" />}
-              >
-                Criar novo usuário
-              </Button>
-            </Link>
-          </Flex>
-        
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px={["4", "4", "6"]} color="gray.300" width="8">
-                  <Checkbox colorScheme="pink" />
-                </Th>
-                <Th>Usuário</Th>
-                { isWiderVersion && (
-                  <Th>Data de cadastro</Th>
-                )}
-                <Th width="8"></Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">André</Text>
-                    <Text fontSize="sm" color="gray.300">andrevictor50@gmail.com</Text>
-                  </Box>
-                </Td>
-                {
-                  isWiderVersion && (
-                    <Td>
-                      04 de abril, 2021
-                    </Td>
-                  )
-                }
-                <Td>
-                  <Button
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="blue"
-                    leftIcon={<Icon as={RiPencilLine} ml="2" fontSize="16"/>}
-                  >
-                    {
-                      isWiderVersion ? 'Editar' : <></>
-                    }
-                  </Button>
-                </Td>
-              </Tr>
-            </Tbody>
-          </Table>
+        <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+          <Sidebar />
+          
+          <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+            <Flex mb="8" justify="space-between" align="center">
+              <Heading size="lg" fontWeight="normal" >Usuários</Heading>
 
-          <Pagination />
-        </Box>
-      </Flex>
-    </Box>
+              <Link href="/users/create" passHref>
+                <Button
+                  as="a"
+                  size="sm"
+                  fontSize="sm"
+                  colorScheme="pink"
+                  leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+                >
+                  Criar novo usuário
+                </Button>
+              </Link>
+            </Flex>
+          
+            <Table colorScheme="whiteAlpha">
+              <Thead>
+                <Tr>
+                  <Th px={["4", "4", "6"]} color="gray.300" width="8">
+                    <Checkbox colorScheme="pink" />
+                  </Th>
+                  <Th>Usuário</Th>
+                  { isWiderVersion && (
+                    <Th>Data de cadastro</Th>
+                  )}
+                  <Th width="8"></Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                <Tr>
+                  <Td px={["4", "4", "6"]}>
+                    <Checkbox colorScheme="pink" />
+                  </Td>
+                  <Td>
+                    <Box>
+                      <Text fontWeight="bold">André</Text>
+                      <Text fontSize="sm" color="gray.300">andrevictor50@gmail.com</Text>
+                    </Box>
+                  </Td>
+                  {
+                    isWiderVersion && (
+                      <Td>
+                        04 de abril, 2021
+                      </Td>
+                    )
+                  }
+                  <Td>
+                    <Button
+                      as="a"
+                      size="sm"
+                      fontSize="sm"
+                      colorScheme="blue"
+                      leftIcon={<Icon as={RiPencilLine} ml="2" fontSize="16"/>}
+                    >
+                      {
+                        isWiderVersion ? 'Editar' : <></>
+                      }
+                    </Button>
+                  </Td>
+                </Tr>
+              </Tbody>
+            </Table>
+
+            <Pagination />
+          </Box>
+        </Flex>
+      </Box>
+    </>
   );
 }
