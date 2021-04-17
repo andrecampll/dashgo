0f815fd063170e23eefcc47b10691e0874351d69
André <andrevictor50@gmail.com>
2021-04-17 12:16:57 -0300

    Listing with React Query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -31,7 +31,20 @@ export default function UserList() {
     const response = await fetch('http://localhost:3000/api/users');
     const data = await response.json();
 
-    return data;
+    const users = data.users.map(user => {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric',
+        }),
+      };
+    });
+
+    return users;
   });
 
   const isWiderVersion = useBreakpointValue({
@@ -91,37 +104,41 @@ export default function UserList() {
                       </Tr>
                     </Thead>
                     <Tbody>
-                      <Tr>
-                        <Td px={["4", "4", "6"]}>
-                          <Checkbox colorScheme="pink" />
-                        </Td>
-                        <Td>
-                          <Box>
-                            <Text fontWeight="bold">André</Text>
-                            <Text fontSize="sm" color="gray.300">andrevictor50@gmail.com</Text>
-                          </Box>
-                        </Td>
-                        {
-                          isWiderVersion && (
-                            <Td>
-                              04 de abril, 2021
+                     {data.map(user => {
+                       return (
+                          <Tr key={user.id}>
+                            <Td px={["4", "4", "6"]}>
+                              <Checkbox colorScheme="pink" />
                             </Td>
-                          )
-                        }
-                        <Td>
-                          <Button
-                            as="a"
-                            size="sm"
-                            fontSize="sm"
-                            colorScheme="blue"
-                            leftIcon={<Icon as={RiPencilLine} ml="2" fontSize="16"/>}
-                          >
+                            <Td>
+                              <Box>
+                                <Text fontWeight="bold">{user.name}</Text>
+                                <Text fontSize="sm" color="gray.300">{user.email}</Text>
+                              </Box>
+                            </Td>
                             {
-                              isWiderVersion ? 'Editar' : <></>
+                              isWiderVersion && (
+                                <Td>
+                                  {user.createdAt}
+                                </Td>
+                              )
                             }
-                          </Button>
-                        </Td>
-                      </Tr>
+                            <Td>
+                              <Button
+                                as="a"
+                                size="sm"
+                                fontSize="sm"
+                                colorScheme="blue"
+                                leftIcon={<Icon as={RiPencilLine} ml="2" fontSize="16"/>}
+                              >
+                                {
+                                  isWiderVersion ? 'Editar' : <></>
+                                }
+                              </Button>
+                            </Td>
+                          </Tr>
+                       )
+                     })}
                     </Tbody>
                   </Table>
 
