303e79e2931b874a322e9b6e300c83d513fd8f8f
André <andrevictor50@gmail.com>
2021-05-08 10:28:05 -0300

    Adding user to context state

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,24 +1,29 @@
+import { useContext } from "react";
 import { Box, Flex, Text, Avatar } from "@chakra-ui/react";
+
+import { AuthContext } from "../../contexts/AuthContext";
 
 type ProfileProps = {
   showProfileData?: boolean;
 }
 
 export function Profile({ showProfileData = true }: ProfileProps) {
+  const { user } = useContext(AuthContext);
+
   return (
     <Flex align="center">
       {
         showProfileData && (
           <Box mr="4" textAlign="right">
-            <Text>André</Text>
+            <Text>{user?.name}</Text>
             <Text color="gray.300" fontSize="small">
-              andrevictor50@gmail.com
+              {user?.email}
             </Text>
           </Box>
         )
       }
 
-      <Avatar size="md" name="André Victor" src="https://github.com/andrecampll.png"/>
+      <Avatar size="md" name={user?.name} />
     </Flex>
   );
 }
